f70b145810147c9a2cea02f8e5432e60ab1cf640
jan.feher <jan.feher@infis.cz>
2023-11-10 09:56:36 +0100

    array skipping

--- src/cycles/RandomIntervals.java
+++ src/cycles/RandomIntervals.java
@@ -1,15 +1,33 @@
 package cycles;
 
+import java.util.Arrays;
+
 public class RandomIntervals
 {
     public static void main(String[] args)
     {
-        int[] random = new int[10];
-        for (int i = 0; i < random.length; i++)
-        {
-            random[i] = (int)(Math.random() * 51 + 50);
-            System.out.println(random[i]);
-        }
+        int[] array = new int[20];
 
+        // Generování čísel
+        for (int i = 0; i < array.length; i++)
+        {
+            array[i] = (int) (Math.random() * 100 - 50);
+        }
+        System.out.println(Arrays.toString(array));
+
+        System.out.println("----");
+
+        int i;
+        int j = array.length - 1;
+        // Seřazování
+        for (i = 0; i <= array.length / 2 - 1; i++) {
+            System.out.println(array[i]);
+            System.out.println(array[j]);
+            j--;
+        }
+        if (array.length % 2 == 1)
+        {
+            System.out.println(array[i]);
+        }
     }
 }
