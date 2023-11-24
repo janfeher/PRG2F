d74a7e42eaf04f3d9868ce47d3dd9a228fed0dd1
jan.feher <jan.feher@infis.cz>
2023-11-24 17:42:20 +0100

    lowest number

--- src/cycles/LowestNumber.java
+++ src/cycles/LowestNumber.java
@@ -1,4 +1,27 @@
 package cycles;
 
-public class LowestNumber {
+import java.util.Arrays;
+
+public class LowestNumber
+{
+    public static void main(String[] args)
+    {
+        int[] pole = new int[50];
+        int minimum;
+
+        for (int i = 0; i < pole.length; i++)
+        {
+            pole[i] = (int)(Math.random() * 200 - 100);
+        }
+        minimum = 100;
+        for (int i = 0; i < pole.length - 1; i++)
+        {
+            if (minimum > pole[i] && pole[i] > 0)
+            {
+                minimum = pole[i];
+            }
+        }
+        System.out.println(Arrays.toString(pole));
+        System.out.println("Nejměnší kladné číslo: " + minimum);
+    }
 }
