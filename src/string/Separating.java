52960baed2abb949d300d370954e902976042a8a
jan.feher <jan.feher@infis.cz>
2023-12-07 12:47:27 +0100

    strings

---
+++ src/string/Separating.java
@@ -0,0 +1,18 @@
+package string;
+
+public class Separating {
+    public static void main(String[] args) {
+        String song = "Dream on;Aerosmith;(1973)";
+        String[] texts = {"first", "second", "another"};
+        System.out.println(texts[1]);
+        String[] split = song.split(";");
+
+        String songName = split[0];
+        String composer = split[1];
+        String yearOfRelease = split[2];
+        System.out.println(songName + ", " + composer + ", " + yearOfRelease);
+
+        String hello = "hello";
+        String something = hello.substring(0, 4);
+    }
+}
