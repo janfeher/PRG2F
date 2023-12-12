3415d50f0e7f898f7dbbab79ff267f401fbca8a0
janfeher <jan.feher@infis.cz>
2023-12-12 13:52:54 +0100

    string

--- src/string/similar_strings.java
+++ src/string/similar_strings.java
@@ -1,20 +1,25 @@
 package string;
 
 import java.lang.String;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class similar_strings {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] strings = {"Jan", "Karel", "Eva", "Franta"};
-        String input = "";
+        String names = "Jan;Karel;Eva;Franta";
+        String[] parts = names.split(";");
+
+        String[] input;
 
         while (true) {
-            input = sc.nextLine();
-            for (int i = 0; i < strings.length; i++) {
-                for (int j = 0; j < input.length(); j++) {
-
-                }
+            input = sc.nextLine().split("");
+            for (int i = 0; i < parts.length; i++) {
+                System.out.println(parts[i]);
+//                for (int j = 0; j < input.length; j++) {
+//                    System.out.println(parts[j]);
+//                }
+//                System.out.println();
             }
         }
     }
