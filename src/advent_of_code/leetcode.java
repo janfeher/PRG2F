91acf05f5c7aeab9d80ea3e1c4bbff13b4a7879a
janfeher <jan.feher@infis.cz>
2023-12-12 13:53:14 +0100

    leetcode

---
+++ src/advent_of_code/leetcode.java
@@ -0,0 +1,23 @@
+package advent_of_code;
+
+public class leetcode {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        nums = new int[]{2, 7, 11, 15};
+        target = 9;
+        int[] result = new int[2];
+
+        for (int i = 0; i != target; i++) {
+            for (int j = 0; j < nums.length; j++) {
+
+            }
+        }
+
+        return result;
+    }
+}
