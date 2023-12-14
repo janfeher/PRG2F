b0862ca38ae944b203e02070293ce6a3d14de2f5
janfeher <jan.feher@infis.cz>
2023-12-14 22:13:41 +0100

    string commenting because bugs

--- src/string/similar_strings.java
+++ src/string/similar_strings.java
@@ -7,19 +7,19 @@ import java.util.Scanner;
 
 public class similar_strings {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String names = "Jan;Karel;Eva;Franta";
-        String[] parts = names.split(";");
-        int[] queue = new
-        char[] chars = new char[];
-
-        String[] input;
-
-        while (true) {
-            input = sc.nextLine().split("");
-            for (int i = 0; i < parts.length; i++) {
-                chars[i] = parts[i].toCharArray();
-            }
-        }
+//        Scanner sc = new Scanner(System.in);
+//        String names = "Jan;Karel;Eva;Franta";
+//        String[] parts = names.split(";");
+//        int[] queue = new
+//        char[] chars = new char[];
+//
+//        String[] input;
+//
+//        while (true) {
+//            input = sc.nextLine().split("");
+//            for (int i = 0; i < parts.length; i++) {
+//                chars[i] = parts[i].toCharArray();
+//            }
+//        }
     }
 }
