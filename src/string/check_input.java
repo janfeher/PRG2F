874eaaf2c27c18cd29701d099aef8a3ce6d3f0a8
jan.feher <jan.feher@infis.cz>
2024-01-12 09:21:25 +0100

    string

--- src/string/check_input.java
+++ src/string/check_input.java
@@ -16,10 +16,21 @@ public class check_input {
         int address_part = Integer.parseInt(parts[i]);
         outer:
         for (i = 0; i < parts.length; i++) {
+            System.out.println(parts[i]);
+            if (!(address_part >= 0 && address_part <= 256)) {
+                System.out.println("Address doesn't have right numbers");
+                break;
+            }
             if (parts.length != 4) {
+                System.out.println("Address is not the right length");
+                break;
+            }
+            if (parts[i].isEmpty()) {
+                System.out.println("Address is missing a number");
                 break;
             }
             for (int j = 0; j < parts[i].length(); j++) {
+
                 char current = parts[i].charAt(j);
                 if (!Character.isDigit(current)) {
                     System.out.println("Address does not contain a number");
--- src/string/similar_strings.java
+++ src/string/similar_strings.java
@@ -2,7 +2,6 @@ package string;
 
 import java.lang.String;
 import java.util.Arrays;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class similar_strings {
