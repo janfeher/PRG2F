0aac5835c581671becb1b9155950e24a319d2bb0
endeavour <jan.feher@infis.cz>
2024-01-26 17:19:03 +0100

    cisla na prvnich radcich se neformatovala spravne

--- src/matrix/tabulka.java
+++ src/matrix/tabulka.java
@@ -6,7 +6,6 @@ import static java.lang.Integer.parseInt;
 
 public class tabulka {
     public static void main(String[] args) {
-        String max = "0";
         String rozsah_i = JOptionPane.showInputDialog("Zadej velikost na ose I:");
         String rozsah_j = JOptionPane.showInputDialog("Zadej velikost na ose J:");
         String max_cislo = JOptionPane.showInputDialog("Zadej maximální číslo:");
@@ -19,10 +18,7 @@ public class tabulka {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
                 array[i][j] = String.valueOf((int) (Math.random() * parseInt(max_cislo) + 1));
-                if (parseInt(array[i][j]) > parseInt(max)) {
-                    max = String.valueOf(array[i][j]);
-                }
-                System.out.print(array[i][j] + " ".repeat(max.length() - array[i][j].length() + 1));
+                System.out.print(array[i][j] + " ".repeat(max_cislo.length() - array[i][j].length() + 1));
             }
             System.out.println();
         }
