42929e82744e9d9a0420d329879ccf6c29f9744d
jan.feher <jan.feher@infis.cz>
2024-03-26 09:34:34 +0100

    pathfinding

--- src/matrix/pathfinding.java
+++ src/matrix/pathfinding.java
@@ -1,21 +1,24 @@
 package matrix;
 
+import java.util.Arrays;
+
 public class pathfinding {
     public static void main(String[] args) {
-        int[][] path = {
-                {2, 1, 1, 1, 0, 0, 0, 0},
-                {0, 0, 0, 1, 0, 0, 0, 0},
-                {0, 0, 0, 1, 1, 1, 1, 0},
-                {0, 0, 0, 0, 0, 0, 1, 0},
-                {0, 0, 0, 0, 0, 0, 1, 0},
-                {0, 1, 1, 1, 1, 1, 1, 0},
-                {0, 1, 0, 0, 0, 0, 0, 0},
-                {0, 1, 1, 1, 1, 1, 1, 3},
-        };
-        for (int i = 0; i < path.length; i++) {
-            for (int j = 0; j < path[i].length; j++) {
+        int[][] map = new int[10][10];
+        int[] start = {0, 0};
+        int[] finish = {4, 4};
 
+        for (int[] ints : map) {
+            Arrays.fill(ints, 0);
+        }
+
+        for (int y = 0; y < map.length; y++) {
+            for (int x = 0; x < map[y].length; x++) {
+                map[start[0]][start[1]] = 1;
+                map[finish[0]][finish[1]] = 2;
+                System.out.print(map[y][x] + "  ");
             }
+            System.out.println();
         }
     }
 }
