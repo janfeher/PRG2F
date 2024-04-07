f37f86bcfc7493e2c89201e8c6ca08ddd09bc82b
janfeher <jan.feher@infis.cz>
2024-04-07 19:34:36 +0200

    leetcode

--- src/advent_of_code/leetcode.java
+++ src/advent_of_code/leetcode.java
@@ -1,23 +1,29 @@
 package advent_of_code;
 
-public class leetcode {
-    public static void main(String[] args) {
+class Solution {
+    public int sequence(int endInt, int price) {
+        int result = 0;
+        for (int i = 0; i <= endInt; i++) {
+            result += i;
+        }
 
+        return result * price;
+    }
+
+    public int sequence(int endInt, int remainder, int price) {
+        int result = sequence(endInt, price);
+
+        return result + (remainder * price);
     }
 }
 
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        nums = new int[]{2, 7, 11, 15};
-        target = 9;
-        int[] result = new int[2];
+public class leetcode {
+    public static void main(String[] args) {
+        int endInt = 9;
+        int remainder = 4;
+        int price = 129;
+        Solution solution = new Solution();
 
-        for (int i = 0; i != target; i++) {
-            for (int j = 0; j < nums.length; j++) {
-
-            }
-        }
-
-        return result;
+        System.out.println("Result: $" + solution.sequence(endInt, remainder, price));
     }
 }
