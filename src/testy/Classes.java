51b3a5e3e251b74ada35a9c9a5279321eaa92988
jan.feher <jan.feher@infis.cz>
2024-04-19 09:21:41 +0200

    test classes

--- src/testy/Classes.java
+++ src/testy/Classes.java
@@ -1,7 +1,60 @@
 package testy;
 
+class ShoppingCart {
+    double totalPrice;
+    double discount;
+
+    ShoppingCart(double discount) {
+        if (discount >= 0 && discount <= 99) {
+            this.discount = (100 - discount) / 100;
+        }
+    }
+
+    void add(int itemPrice) {
+        totalPrice += itemPrice;
+    }
+
+    void remove(int itemPrice) {
+        if (totalPrice >= itemPrice) {
+            totalPrice -= itemPrice;
+        }
+    }
+
+    double getPrice() {
+        return totalPrice * discount * 1.21;
+    }
+}
+
+class RentableLand {
+    final int COST_PER_SQUARE_METER = 10;
+    boolean farming;
+
+    RentableLand(boolean farming) {
+        this.farming = farming;
+    }
+
+    int rent(int width, int length, int days) {
+        int size = width * length;
+        int price = COST_PER_SQUARE_METER * size * days;
+        if (farming && days > 182 && price >= 100000) {
+            return price - 100000;
+        }
+        return price;
+    }
+}
+
 public class Classes {
     public static void main(String[] args) {
+        ShoppingCart cart = new ShoppingCart(30);
+        cart.add(150);
+        cart.remove(50);
+        double totalPrice = cart.getPrice();
+        System.out.println(totalPrice);
 
+        System.out.println("----------------");
+
+        RentableLand land = new RentableLand(true);
+        int rentPrice = land.rent(10, 10, 30);
+        System.out.println(rentPrice);
     }
 }
