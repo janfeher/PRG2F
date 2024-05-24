f5a6b085060e8cb9c7f7ab412928a73772821e69
janfeher <jan.feher@infis.cz>
2024-05-24 13:55:33 +0200

    add dealer ai, enhanced ui

--- src/oop/referencing/BlackJack.java
+++ src/oop/referencing/BlackJack.java
@@ -1,6 +1,7 @@
 package oop.referencing;
 
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class BlackJack {
     Player player;
@@ -13,7 +14,6 @@ public class BlackJack {
     Scanner sc = new Scanner(System.in);
     int input;
     int bet;
-
     void start() {
         System.out.println("Balance: " + player.balance);
         do {
@@ -27,11 +27,10 @@ public class BlackJack {
     int[] cards = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10};
     int cardIndex;
     int cardValue;
-
-    void pickACard() {
+    int pickedCard() {
         cardIndex = (int) (Math.random() * cards.length);
         cardValue = cards[cardIndex];
-        player.handValue += cardValue;
+        return cardValue;
     }
 
     int evaluate() {
@@ -43,33 +42,66 @@ public class BlackJack {
         return 1;
     }
 
-    void gameOver() {
-        if (evaluate() == 0) {
+    int evaluateDealer() {
+        if (player.handValue > 21) {
+            return 0;
+        } else if (player.handValue == 21) {
+            return 2;
+        }
+        return 1;
+    }
+
+    void stand() throws InterruptedException {
+        while (dealerValue < 17) {
+            TimeUnit.MILLISECONDS.sleep(500);
+            dealerValue += pickedCard();
+            System.out.println("Value: " + cardValue);
+        }
+    }
+
+    void gameOver(int result) {
+        if (result == 0) {
             System.out.println("You lost!");
-        } else if (evaluate() == 2) {
+        } else if (result == 2) {
             System.out.println("You won " + bet + " coins");
             player.balance += bet;
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Player p = new Player("Karel", 1000);
         BlackJack game = new BlackJack(p);
 
-        game.start();
         Scanner sc = new Scanner(System.in);
-        int option;
-        do {
-            if (game.evaluate() == 1) {
+        int option = 1;
+        while (option == 1){
+            game.start();
+            game.player.handValue = 0;
+            game.dealerValue = 0;
+            while (game.evaluate() == 1) {
                 System.out.print("Enter your option: ");
                 option = sc.nextInt();
-                game.pickACard();
-                System.out.println("Value of the current card: " + game.cardValue);
-                System.out.println("Value in hand: " + game.player.handValue);
-            } else {
-                game.gameOver();
-                break;
+                if (option == 1) {
+                    game.player.handValue += game.pickedCard();
+                    System.out.println("Value of the current card: " + game.cardValue);
+                    System.out.println("Value in hand: " + game.player.handValue);
+                } else if (option == 2) {
+                    game.stand();
+                    break;
+                }
             }
-        } while (option == 1);
+            if (game.evaluate() == 1) {
+                System.out.println("Dealer's hand: " + game.dealerValue);
+                if (game.dealerValue > 21 || game.dealerValue < game.player.handValue) {
+                    game.gameOver(2);
+                } else {
+                    game.gameOver(0);
+                }
+            } else {
+                game.gameOver(game.evaluate());
+            }
+            System.out.println("Do you want to play a new game?");
+            option = sc.nextInt();
+        }
     }
 }
